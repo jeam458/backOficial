355adaaa9569d5c9d7f0f86134ca28476358402c
sansprok <miguelsantosprado@outlook.es>
2022-01-29 21:28:41 -0500

    Mantenimiento 20220000022201

--- models/registros.js
+++ models/registros.js
@@ -3,7 +3,9 @@ var AccesosSchema = new mongoose.Schema({
     CodUsuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuarios' },
     NombreUsuario: String,
     Ip:String,
-    fecha: { type: Date, default: Date.now },
+    fecha: { 
+        type: Date, default: Date.now 
+    },
     fecha2: { type: Date },
     lat: Number,
     lng: Number
