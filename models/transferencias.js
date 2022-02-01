7d59e3c35f488c54adb7a06195c80135f7be571f
sansprok <miguelsantosprado@outlook.es>
2022-01-31 22:04:41 -0500

    Mantenimiento 20220000012201

--- models/transferencias.js
+++ models/transferencias.js
@@ -13,7 +13,9 @@ var TransferenciasSchema = new mongoose.Schema({
     Suc1Nombre: String,
     Sucursal2: { type: mongoose.Schema.Types.ObjectId, ref: 'Sucursales' },
     Suc2Nombre: String,
-    fecha: { type: Date, default: Date.now },
+    fecha: { 
+        type: Date, default: Date.now 
+    },
     Estado: Number,
     fecha2: { type: Date },
     fecha3: { type: Date },
